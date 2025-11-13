80d2711d151633fdd8b45fa3fc99038884ac9402
xyeison2010 <leonardohuacahidalgo@gmail.com>
2025-11-12 21:59:53 -0500

    change2

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,24 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { HashRouter, BrowserRouter, Route, Switch } from 'react-router-dom';
 
-import "./App.css";
+import './App.css';
 import Home from './containers/Home/Home';
 
 import NotFound from './containers/NotFound/NotFound';
-
 
 class App extends React.Component {
   render() {
     return (
       <>
-        <BrowserRouter>
-         
-            <Switch>
-              <Route exact path="/" component={Home} />
-            
-              <Route component={NotFound} /> 
-            </Switch>
-        
-        </BrowserRouter>
+        <HashRouter>
+          <Switch>
+            <Route exact path="/" component={Home} />
+
+            <Route component={NotFound} />
+          </Switch>
+        </HashRouter>
       </>
     );
   }
-}//falta jugar torneo hacer
-//ese  component={NotFound} significa cuando nos dirigimos a un url no descrita
+}
 export default App;
